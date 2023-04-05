5cbfc65b34207dc82f255c4adfe7e819f82875df
youknowHRT <1021300769@qq.com>
2023-04-05 20:50:08 +0800

    feat: 429 拦截

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -26,7 +26,7 @@ export class Http {
 export const http = new Http('/api/v1')
 
 http.instance.interceptors.request.use(
-  function (config) {
+  (config) => {
     // 在发送请求之前做些什么
     console.log(config, '🍋')
     if (config._autoLoading === true) {
@@ -38,25 +38,28 @@ http.instance.interceptors.request.use(
     }
     return config
   },
-  function (error) {
+  (error) => {
     // 对请求错误做些什么
     return Promise.reject(error)
   }
 )
 
 http.instance.interceptors.response.use(
-  function (response) {
+  (response) => {
     console.log(response, '🍎')
     if (response.config._autoLoading === true) {
       closeToast()
     }
     return response
   },
-  function (error) {
+  (error) => {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
     if (error.config._autoLoading === true) {
       closeToast()
+    }
+    if (error.response?.status === 429) {
+      alert('你太频繁了')
     }
     return Promise.reject(error)
   }
